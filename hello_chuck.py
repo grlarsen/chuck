5001e7d0ffe62de97470c402969e416568196444
grlarsen <gregor.larsen@gmail.com>
2024-05-16 11:34:52 +0200

    modified time

--- hello_chuck.py
+++ hello_chuck.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import requests as re
+import time
 
-res = re.get(url='https://api.chucknorris.io/jokes/random')
-res = res.json()['value']
-print(res)
+def chuck(i):
+    res = re.get(url='https://api.chucknorris.io/jokes/random')
+    res = res.json()['value']
+    print(i, res)
+
+if __name__=='__main__':
+    for i in range(100):
+        chuck(i)
+        time.sleep(1)
